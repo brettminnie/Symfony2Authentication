e786d75c86c091c2288346a1ed035d8197ac4c82
Brett Minnie <github@bdb-studios.co.uk>
2014-08-29 10:19:38 +0100

    Added FOSUserBundle & created user entity

---
+++ src/BDBStudios/AuthBundle/BDBStudiosAuthBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BDBStudios\AuthBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BDBStudiosAuthBundle extends Bundle
+{
+}
---
+++ src/BDBStudios/AuthBundle/Entity/User.php
@@ -0,0 +1,89 @@
+<?php
+
+
+namespace BDBStudios\AuthBundle\Entity;
+
+use \FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="authentication_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    protected $surname;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+
+
+}
